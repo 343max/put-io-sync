af354135a6514babfeb9cc4ef7f3ca36963bd80e
Max Winde <max.winde@gmail.com>
2013-03-20 16:04:09 +0100

    reading command line arguments

--- sync.js
+++ sync.js
@@ -1,7 +1,25 @@
 var PutIO = require('put.io-v2');
 
 var api = new PutIO('CI0698O7');
+var argv = require( 'argv' );
 
-api.files.list(0, function(data) {
-  console.dir(data);
-});
+var args = argv.option([{
+  name: 'directory-id',
+  short: 'd',
+  type: 'int',
+  description: 'id of the directory to sync'
+}]).run();
+
+var directoryId = args.options['directory-id'] || 0;
+
+//console.dir(args);
+console.dir(directoryId);
+//
+
+function listDir(directoryId) {
+  api.files.list(directoryId, function(data) {
+    console.dir(data.files);
+  });
+}
+
+listDir(directoryId);

497b67be86fa5e5cad04c8e298a9adb7ceddb287
Max Winde <max.winde@gmail.com>
2013-03-20 16:11:47 +0100

    recursively creating directories

--- sync.js
+++ sync.js
@@ -1,25 +1,43 @@
 var PutIO = require('put.io-v2');
+var argv = require( 'argv' );
+var _ = require('underscore');
+var fs = require('fs');
 
 var api = new PutIO('CI0698O7');
-var argv = require( 'argv' );
 
 var args = argv.option([{
   name: 'directory-id',
   short: 'd',
   type: 'int',
   description: 'id of the directory to sync'
+}, {
+  name: 'local-path',
+  short: 'l',
+  type: 'path',
+  description: 'local filepath to sync to'
 }]).run();
 
 var directoryId = args.options['directory-id'] || 0;
+var localPath = args.options['local-path'];
 
 //console.dir(args);
 console.dir(directoryId);
 //
 
-function listDir(directoryId) {
-  api.files.list(directoryId, function(data) {
-    console.dir(data.files);
+function listDir(directoryId, localPath) {
+  console.log(localPath);
+
+  fs.mkdir(localPath, 0766, function() {
+    api.files.list(directoryId, function(data) {
+      _.each(data.files, function(node) {
+        console.dir(node);
+
+        if (node.content_type == 'application/x-directory') {
+          listDir(node.id, localPath + '/' + node.name);
+        }
+      });
+    });
   });
 }
 
-listDir(directoryId);
+listDir(directoryId, localPath);
